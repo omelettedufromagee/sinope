2a823e0bce45ed510341435e49727530f8f6c98e
Lavren <lavrenti@rogoff.fr>
2017-11-05 13:56:10 -0500

    Adding sinope.py component

---
+++ custom_components/climate/sinope.py
@@ -0,0 +1,245 @@
+"""
+Support for Sinope.
+
+For more details about this platform, please refer to the documentation at
+"""
+import logging
+from datetime import timedelta
+
+import requests
+import voluptuous as vol
+import json
+import re
+
+import homeassistant.helpers.config_validation as cv
+from homeassistant.components.climate import (ClimateDevice, PLATFORM_SCHEMA, STATE_HEAT, STATE_IDLE, ATTR_TEMPERATURE, ATTR_AWAY_MODE)
+from homeassistant.const import (CONF_USERNAME, CONF_PASSWORD, CONF_NAME, TEMP_CELSIUS)
+
+_LOGGER = logging.getLogger(__name__)
+
+DEFAULT_NAME = 'Sinope'
+
+REQUESTS_TIMEOUT = 15
+
+HOST = "https://neviweb.com"
+LOGIN_URL = "{}/api/login".format(HOST)
+GATEWAY_URL = "{}/api/gateway".format(HOST)
+GATEWAY_DEVICE_URL = "{}/api/device?gatewayId=".format(HOST)
+DEVICE_DATA_URL = "{}/api/device/".format(HOST)
+
+PLATFORM_SCHEMA = PLATFORM_SCHEMA.extend({
+    vol.Required(CONF_USERNAME): cv.string,
+    vol.Required(CONF_PASSWORD): cv.string,
+    vol.Optional(CONF_NAME, default=DEFAULT_NAME): cv.string
+})
+
+
+def setup_platform(hass, config, add_devices, discovery_info=None):
+    """Set up the Sinope sensor."""
+    username = config.get(CONF_USERNAME)
+    password = config.get(CONF_PASSWORD)
+    gateway = config.get("gateway")
+
+    try:
+        sinope_data = SinopeData(username, password, gateway)
+        sinope_data.update()
+    except requests.exceptions.HTTPError as error:
+        _LOGGER.error("Failt login: %s", error)
+        return False
+
+    name = config.get(CONF_NAME)
+    
+    # for device in data:
+        # print("Room: {}".format(device["name"]))
+        # print("Id: {}".format(device["id"]))
+        # print("Wattage: {}".format(device["wattage"]))
+        # print("Set Point: {}".format(device["info"]["setpoint"]))
+        # print("Temperature: {}".format(device["info"]["temperature"]))
+        # print("\n")
+
+    devices = []
+    for id, device in sinope_data.data.items():
+        devices.append(SinopeThermostat(sinope_data, id, '{} {}'.format(name, device["info"]["name"])))
+
+    add_devices(devices, True)
+
+class SinopeThermostat(ClimateDevice):
+    """Implementation of a Sinope Device."""
+
+    def __init__(self, sinope_data, device_id, name):
+        """Initialize."""
+        self.client_name = name
+        self.client = sinope_data.client
+        self.device_id = device_id
+        self.sinope_data = sinope_data
+
+        self._target_temp  = None
+        self._cur_temp = None
+        self._min_temp  = float(self.sinope_data.data[self.device_id]["info"]["tempMin"])
+        self._max_temp  = float(self.sinope_data.data[self.device_id]["info"]["tempMax"])
+        self._mode = None
+        self._state = None
+        self._away = False
+
+    def update(self):
+        """Get the latest data from Sinope and update the state."""
+        self.sinope_data.update()
+        self._target_temp  = float(self.sinope_data.data[self.device_id]["data"]["setpoint"])
+        self._cur_temp =  float(self.sinope_data.data[self.device_id]["data"]["temperature"])
+        self._mode = float(self.sinope_data.data[self.device_id]["data"]["mode"])
+        self._state = float(self.sinope_data.data[self.device_id]["data"]["heatLevel"])
+
+    @property
+    def name(self):
+        """Return the name of the sinope, if any."""
+        return self.client_name
+
+    @property
+    def temperature_unit(self):
+        """Return the unit of measurement."""
+        return TEMP_CELSIUS
+
+    @property
+    def target_temperature (self):
+        """Return the temperature we try to reach."""
+        return self._target_temp
+
+    def set_temperature(self, **kwargs):
+        """Set new target temperature."""
+        temperature = kwargs.get(ATTR_TEMPERATURE)
+        if temperature is None:
+            return
+        self.client.set_temperature_device(self.device_id, temperature)
+        self._target_temp = temperature
+
+    @property
+    def current_temperature(self):
+        """Return the current temperature."""
+        return self._cur_temp
+
+    @property
+    def min_temp(self):
+        """Return the min temperature."""
+        return self._min_temp
+
+    @property
+    def max_temp(self):
+        """Return the max temperature."""
+        return self._max_temp
+
+    @property
+    def current_operation(self):
+        """Return current operation i.e. heat, cool, idle."""
+        if self._state:
+            return STATE_HEAT
+        return STATE_IDLE
+
+    def mode(self):
+        return self._mode
+
+class SinopeData(object):
+
+    def __init__(self, username, password, gateway):
+        """Initialize the data object."""
+        self.client = SinopeClient(username, password, gateway, REQUESTS_TIMEOUT)
+        self.data = {}
+
+    def update(self):
+        """Get the latest data from Sinope."""
+        try:
+            self.client.fetch_data()
+        except PySinopeError as exp:
+            _LOGGER.error("Error on receive last Sinope data: %s", exp)
+            return
+        self.data = self.client.get_data()
+
+class PySinopeError(Exception):
+    pass
+
+
+class SinopeClient(object):
+
+    def __init__(self, username, password, gateway, timeout=REQUESTS_TIMEOUT):
+        """Initialize the client object."""
+        self.username = username
+        self.password = password
+        self._headers = None
+        self.gateway = gateway
+        self.gateway_id = None
+        self._data = {}
+        self._gateway_data = {}
+        self._cookies = None
+        self._timeout = timeout
+		
+        self._post_login_page()
+        self._get_data_gateway()
+
+    def _post_login_page(self):
+        """Login to Sinope website."""
+        data = {"email": self.username, "password": self.password, "stayConnected": 1}
+        try:
+            raw_res = requests.post(LOGIN_URL, data=data, cookies=self._cookies, allow_redirects=False, timeout=self._timeout)
+        except OSError:
+            raise PySinopeError("Can not submit login form")
+        if raw_res.status_code != 200:
+            raise PySinopeError("Cannot log in")
+
+        # Update session
+        self._cookies = raw_res.cookies
+        self._headers = {"Session-Id": raw_res.json()["session"]}
+        return True
+
+    def _get_data_gateway(self):
+        """Get gateway data."""
+        # Prepare return
+        data = {}
+        # Http request
+        try:
+            raw_res = requests.get(GATEWAY_URL, headers=self._headers, cookies=self._cookies, timeout=self._timeout)
+            gateways = raw_res.json()
+
+            for gateway in gateways:
+                if gateway["name"] == self.gateway:
+                    self.gateway_id = gateway["id"]
+                    break
+            raw_res = requests.get(GATEWAY_DEVICE_URL + str(self.gateway_id), headers=self._headers, cookies=self._cookies, timeout=self._timeout)
+        except OSError:
+            raise PySinopeError("Can not get page")
+        # Update cookies
+        self._cookies.update(raw_res.cookies)
+        # Prepare data
+        self._gateway_data = raw_res.json()
+
+    def _get_data_device(self, device):
+        """Get device data."""
+        # Prepare return
+        data = {}
+        # Http request
+        try:
+            raw_res = requests.get(DEVICE_DATA_URL + str(device) + "/data", headers=self._headers, cookies=self._cookies, timeout=self._timeout)
+        except OSError:
+            raise PySinopeError("Can not get page")
+        # Update cookies
+        self._cookies.update(raw_res.cookies)
+        # Prepare data
+        data = raw_res.json()
+        return data
+
+    def fetch_data(self):
+        sinope_data = {}
+        # Get data each device
+        for device in self._gateway_data:
+            sinope_data.update({ device["id"] : { "info" : device, "data" : self._get_data_device(device["id"]) }})
+        self._data = sinope_data
+
+    def get_data(self):
+        """Return collected data"""
+        return self._data
+
+    def set_temperature_device(self, device, temperature):
+        """Set device temperature."""
+        data = {"temperature": temperature}
+        try:
+            raw_res = requests.put(DEVICE_DATA_URL + str(device) + "/setpoint", data=data, headers=self._headers, cookies=self._cookies, timeout=self._timeout)
+        except OSError:
+            raise PySinopeError("Cannot set device temperature")
